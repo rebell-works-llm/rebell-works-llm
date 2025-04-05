6a605da8ca54460d6ab6df0b6ab226b2ed1860b7
Gekke-SuCro <jayden.roeper@student.hu.nl>
2025-04-05 21:41:10 +0200

    feat: add vacancy service with fake JSON implementation

---
+++ backend/src/main/java/com/rebellworksllm/backend/matching/application/DummyVacancyService.java
@@ -0,0 +1,51 @@
+package com.rebellworksllm.backend.matching.application;
+
+import com.rebellworksllm.backend.matching.application.exception.CannotFetchVacancyEmbeddingsException;
+import com.rebellworksllm.backend.matching.domain.Vacancy;
+import com.rebellworksllm.backend.matching.domain.VacancyService;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DummyVacancyService implements VacancyService {
+
+    private static final String JSON_FILE_PATH = "vector-vacancies.json";
+
+    @Override
+    public List<Vacancy> getVacancies() {
+        List<Vacancy> vacancies = new ArrayList<>();
+        for (Object object : readJsonFile()) {
+            JSONObject vacancy = (JSONObject) object;
+
+            JSONArray embeddingArray = (JSONArray) vacancy.get("embedding");
+            float[] vector = new float[embeddingArray.size()];
+            for (int i = 0; i < embeddingArray.size(); i++) {
+                vector[i] = ((Number) embeddingArray.get(i)).floatValue();
+            }
+
+            vacancies.add(new Vacancy(vacancy.get("title").toString(), vector));
+        }
+
+        return vacancies;
+    }
+
+    private JSONArray readJsonFile() {
+        try {
+            Object obj = new JSONParser().parse(new FileReader(JSON_FILE_PATH));
+            JSONObject jsonObject = (JSONObject) obj;
+
+            return (JSONArray) jsonObject.get("dummy-vacancies");
+        } catch (IOException | ParseException e) {
+            throw new CannotFetchVacancyEmbeddingsException("Cannot fetch vacancy embeddings from file'"
+                    + DummyVacancyService.JSON_FILE_PATH + "': " + e.getMessage());
+        }
+    }
+}
