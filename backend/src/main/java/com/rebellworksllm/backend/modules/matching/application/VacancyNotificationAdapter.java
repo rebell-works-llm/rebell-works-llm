55768ed6e851b4cd330f6279ad027a73bd5ec27c
Kevin <Kevin.koot@student.hu.nl>
2025-06-22 23:01:50 +0200

    Log toegevoegd

--- backend/src/main/java/com/rebellworksllm/backend/modules/matching/application/VacancyNotificationAdapter.java
+++ backend/src/main/java/com/rebellworksllm/backend/modules/matching/application/VacancyNotificationAdapter.java
@@ -3,12 +3,15 @@ package com.rebellworksllm.backend.modules.matching.application;
 import com.rebellworksllm.backend.modules.matching.domain.Vacancy;
 import com.rebellworksllm.backend.modules.vacancies.application.dto.VacancyResponseDto;
 import com.rebellworksllm.backend.modules.whatsapp.domain.WhatsAppService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
 
 public class VacancyNotificationAdapter {
 
+    private static final Logger logger = LoggerFactory.getLogger(HubSpotWebhookService.class);
     private final WhatsAppService whatsAppService;
     private final TemplateService templateOne;
     private final TemplateService templateTwo;
@@ -31,6 +34,7 @@ public class VacancyNotificationAdapter {
         Vacancy vacancy3 = new Vacancy(vac3.id(), vac3.title(), vac3.description(), vac3.salary(), vac3.workingHours(), vac3.function());
         Vacancy vacancy4 = new Vacancy(vac4.id(), vac4.title(), vac4.description(), vac4.salary(), vac4.workingHours(), vac4.function());
 
+        logger.info("vacancy 4 {}", vacancy4);
 
         List<String> parameters = templateTwo.generateVacancyTemplateParams("Student", vacancy3, vacancy4);
         whatsAppService.sendTemplateMessage(phoneNumber, "rebell_template2", "nl", parameters);
