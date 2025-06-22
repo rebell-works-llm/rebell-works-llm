d44b86887aadcd58ef63687707487962a6be6b05
Kevin <Kevin.koot@student.hu.nl>
2025-06-22 23:49:51 +0200

    Velden supabaseresponse uitgebreid, zodat deze gevuld worden

--- backend/src/main/java/com/rebellworksllm/backend/modules/vacancies/data/SupabaseResponse.java
+++ backend/src/main/java/com/rebellworksllm/backend/modules/vacancies/data/SupabaseResponse.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record SupabaseResponse(
         String id,
         String description,
+        String salary,
+        String workingHours,
+        String function,
         double priority,
         @JsonProperty("match_count") int matchCount,
         String link,
